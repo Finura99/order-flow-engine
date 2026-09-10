835c153237868cf45cc5907af878f9a2b382955d
Arya Elpula <aryaelpula@gmail.com>
2026-09-10 18:10:58 +0200

    Add business rules using validations

--- src/main/java/com/arya/orderflow/Main.java
+++ src/main/java/com/arya/orderflow/Main.java
@@ -6,14 +6,24 @@ public class Main {
         System.out.println("Order FLow Engine started");
 
         Order order = new Order(1, "AAPL", OrderSide.BUY, 100, 100.50);
+        // Create the trading ticket
 
-        System.out.println("Order ID: " + order.getId());
-        System.out.println("Symbol: " + order.getSymbol());
-        System.out.println("Side: " + order.getOrderSide());
-        System.out.println("Quantity: " + order.getQuantity());
-        System.out.println("Price: " + order.getPrice());
-        System.out.println("Status: " + order.getStatus());
+        OrderValidator validator = new OrderValidator();
+
+        Order validOrder = new Order(1, "AAPL", OrderSide.BUY, 100, 190.50);
+        boolean validOrderResult = validator.isValid(validOrder);
+        // Checks if its valid (Defensive programming)
+
+        System.out.println("Valid Order result: " + validOrderResult);
+
+        Order badOrder = new Order(1, "", OrderSide.BUY, -100, 190.50);
+        boolean badOrderResult = validator.isValid(badOrder);
+        // Asks if the order is valid
+
+        System.out.println("Bad order result: " + badOrderResult);
+        
     }
 }
 
+// For now, main acts as a mini-orchestrator
 
